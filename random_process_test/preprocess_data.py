cc001d6f6e679c16a49ac7f88ec2967c77d47026
lingysjustinas@gmail.com <lingysjustinas@gmail.com>
2017-02-23 07:56:02 -0800

    Almost done with the text processing script.

--- random_process_test/preprocess_data.py
+++ random_process_test/preprocess_data.py
@@ -16,24 +16,130 @@ INFO_MSG = "\nUsage: {0} -dir_path path_value [-perc int_value]\n"
 TEXT_TITLE_FRAME = "temp_stage_{0}_prc_{1}.txt"
 
 
+MAX_PERCENT = 100
+MIN_PERCENT = 1
 
 
-def _handle_originals(files, stage, prc_id, percent):
-    pass
+def _handle_originals(dir_path, d_files, stage, prc_id, percent):
 
 
-def _handle_temps(files, stage, prc_id):
-    pass
+    if len(d_files) == 0 or len(d_files) == 1: # nothing to combine
+        return
+
+    # there are two cases:
+    #    1. percent < 100;
+    #    2. percent == 100.
+
+    if percent == MAX_PERCENT: # read full files but
+                               # ignore the first line
+
+        # create a new file for combining all other values
+        temp_file = os.path.join(dir_path,
+                TEXT_TITLE_FRAME.format(stage, prc_id))
 
 
 
+        with open(temp_file, "w") as w_file:
 
-def _process_data(path_to_dir, percent=100):
+            # loop through all passed files
+            # and process them
+            for r_file in d_files:
+
+                skip_line = True
+                with open(os.path.join(dir_path, r_file), "r") as fd:
+                    for line in fd:
+                        if skip_line: # ignore first line
+                            skip_line = False
+                            continue
+
+                        # write a line to the w_file
+                        w_file.write(line)
+
+
+    else: # only last few lines should be read
+
+
+        percent = percent / 100.0
+        # create a new file for combining all other values
+        temp_file = os.path.join(dir_path,
+                TEXT_TITLE_FRAME.format(stage, prc_id))
+
+        with open(temp_file, "w") as w_file:
+
+            # loop through all passed files
+            # and process them
+            for r_file in d_files:
+
+                file_size = os.path.getsize(os.path.join(dir_path, r_file))
+                start_read_from = int(file_size*percent)
+                skip_line = True
+
+                with open(os.path.join(dir_path, r_file), "r") as fd:
+                    fd.seek(-start_read_from, 2) # go to this offset
+                                                 # before the end of
+                                                 # the file
+
+                    for line in fd:
+                        if skip_line: # ignore first line
+                            skip_line = False
+                            continue
+
+                        # write a line to the w_file
+                        w_file.write(line)
+
+
+
+'''
+Function handles intermediate files so that merges previous stage
+files and deletes them.
+'''
+def _handle_temps(dir_path, cmb_files, stage, prc_id):
+
+    if cmb_files[0] == cmb_files[1]: # means nothing to combine
+        return
+
+    # all stages follow the same title pattern. So, this function
+    # has to read files from previous stages passed in cmb_files
+    # and after reading them delete them -- save storage.
+
+    temp_file = os.path.join(dir_path,
+            TEXT_TITLE_FRAME.format(stage, prc_id))
+
+    beg_range = cmb_files[0] # beginning of the range
+    end_range = cmb_files[1] # the end of range file
+
+
+    with open(temp_file, "w") as w_file:
+
+
+        for cur_f_range in xrange(beg_range, end_range + 1, 1):
+            # just open, read and delete files from
+            # previous stage
+            read_temp = os.path.join(dir_path,
+                TEXT_TITLE_FRAME.format(stage-1, cur_f_range))
+
+            with open(read_temp, "r") as fd:
+                # read line by line
+                # and append it to the new
+                # open file
+                for line in fd:
+                    w_file.write(line)
+
+            # file has been read and closed
+            # now need to delete it
+            os.remove(read_temp)
+
+
+
+def _process_data(path_to_dir, percent=MAX_PERCENT):
 
     # check percentage first
-    if percent < 0 or percent > 100:
-        raise ValueError("\nPercentage value must be within [0, 100]\n")
+    if percent < MIN_PERCENT or percent > MAX_PERCENT:
+        raise ValueError("\nPercentage value must be in [{0}, {1}]\n".format(MIN_PERCENT, MAX_PERCENT))
 
+    # check if the path is a directory
+    if not os.path.isdir(path_to_dir):
+        raise ValueError("\n'{0}' is not a directory\n".format(path_to_dir))
 
     # get references to the files that have to be processed
     # These references will be used later by processes.
@@ -46,9 +152,6 @@ def _process_data(path_to_dir, percent=100):
 
     if total_files == 0 or CPU_COUNT == 0: # nothing to process
         return
-
-
-    #print "---- Some file size: {0} bytes ----".format(os.path.getsize(os.path.join(path_to_dir, data_files[0])))
 
 
 
@@ -74,7 +177,8 @@ def _process_data(path_to_dir, percent=100):
     for prc_idx in xrange(0, CPU_COUNT - 1, 1):
         results[prc_idx] = prc_pool.apply_async(
                 _handle_originals,
-                (data_files[global_index:global_index+files_per_prc:1],
+                (path_to_dir,
+                data_files[global_index:global_index+files_per_prc:1],
                 1, prc_idx, percent))
 
         global_index += files_per_prc
@@ -82,14 +186,63 @@ def _process_data(path_to_dir, percent=100):
 
     # last process handles the rest
     results[CPU_COUNT - 1] = prc_pool.apply_async(_handle_originals,
-            (data_files[global_index::1], 1, CPU_COUNT-1, percent))
+            (path_to_dir,
+            data_files[global_index::1],
+            1, CPU_COUNT-1, percent))
+
+
+
+    # wait for all processes to complete
+    for prc_state in results:
+        prc_state.wait()      # means all processes have completed
 
 
     # original files completed
     # now only merging left
+    left_files = CPU_COUNT # current number of intermediate files
+    stage_value = 2        # first stage was finished above
+
+
+    while left_files != 1: # keep merging until work only for one
+                           # process is left
+
+        num_cpus = left_files / 2 # cal num of cpus to use
+
+        files_per_proc = left_files / num_cpus
+
+        global_index = 0
+
+        # similar to the above code
+        for prc_idx in xrange(0, num_cpus - 1, 1):
+            results[prc_idx] = prc_pool.apply_async(
+                    _handle_temps, (path_to_dir,
+                    (global_index, global_index + files_per_proc - 1),
+                    stage_value, prc_idx))
+
+            global_index += files_per_proc
+
+
+        # for the last process, deal with the remaining files
+        results[num_cpus-1] = prc_pool.apply_async(
+                _handle_temps, (path_to_dir,
+                (global_index, left_files - 1),
+                stage_value, num_cpus-1))
+
+        # update all constants
+        stage_value += 1 # increment the stage
+        left_files = num_cpus # new value of remaining vals
+
+        # now wait until all processes complete
+        for wait_idx in xrange(0, num_cpus, 1):
+            results[wait_idx].wait()
+
+
+        # restart the while loop
 
 
 
+    # all data files have been merged into
+    # one big data file
 
     prc_pool.close() # close any incoming tasks
     prc_pool.join()  # wait when the pool terminates
@@ -116,7 +269,7 @@ def _process_input(inputs):
     if inputs[1] == "-dir_path" and num_params > 1: # first case
 
             if num_params == 2:
-                return (inputs[2], 100) # default value
+                return (inputs[2], MAX_PERCENT) # default value
 
 
             # below cases handle percent part
@@ -170,9 +323,9 @@ if __name__ == "__main__":
 
     else:
         # check inputs
+
         try:
             path_name, percentage = _process_input(sys.argv)
-
             # start processing files in the directory
             _process_data(path_to_dir=path_name, percent=percentage)
 
